222d415ed8f92a0db8292de6401e21fc62355ee8
BuildTools <magneticchen@gmail.com>
2020-12-15 18:35:57 +0800

    fix term

--- src/browser/editor.js
+++ src/browser/editor.js
@@ -9,244 +9,8 @@ function saveByteArray(file_name, byte) {
     link.click();
 };
 
-function NoteUnitRow(settings) {
-  this._note_code_int = settings.note_code_int;
-  this._tone_duration_millis = settings.tone_duration_millis;
-  this._rest_duration_millis = settings.rest_duration_millis;
-  this._button_instruction_list = settings.button_instruction_list;
-  this._button_instruction_checkbox_list = [];
-  this._play_k_notes_int = settings.play_k_notes_int;
-  this._audio_context = settings.audio_context;
-  this._insert_before = settings.insert_before;
+function ButtonInstructionLayer(settings) {
   this._time_unit = settings.time_unit;
-  this._to_be_remove = false;
-}
-
-NoteUnitRow.prototype.initialize = function(animation_container_dom_object, daf)  {
-  this._animation_container_dom_object = animation_container_dom_object;
-  this._dom_object = document.createElement("tr");
-  // this._dom_object.className = "";
-  const th_0 = document.createElement("th");
-  const select_0 = document.createElement("select");
-  for(let note_code_int in Notes) {
-    const option = document.createElement("option");
-    option.text = Notes[note_code_int][1];
-    option.value = note_code_int;
-    select_0.add(option);
-  }
-
-  select_0.selectedIndex = this._note_code_int;
-
-  select_0.addEventListener('change', (event) => {
-    this._note_code_int = event.target.value;
-  });
-  th_0.appendChild(select_0);
-
-  const th_1 = document.createElement("th");
-  const input_1 = document.createElement("input");
-  input_1.setAttribute("type", "number");
-  input_1.setAttribute("min", 1);
-  input_1.setAttribute("max", 256);
-  input_1.setAttribute("value", this._tone_duration_millis);
-  input_1.addEventListener('change', (event) => {
-    this._tone_duration_millis = event.target.value;
-  });
-  th_1.appendChild(input_1);
-
-  const th_2 = document.createElement("th");
-  const input_2 = document.createElement("input");
-  input_2.setAttribute("type", "number");
-  input_2.setAttribute("min", 0);
-  input_2.setAttribute("max", 255);
-  input_2.setAttribute("value", this._rest_duration_millis);
-  input_2.addEventListener('change', (event) => {
-    this._rest_duration_millis = event.target.value;
-  });
-  th_2.appendChild(input_2);
-
-  const th_3 = document.createElement("th");
-  const check_box_3_0 = document.createElement("input");
-  check_box_3_0.setAttribute("type", "checkbox");
-  const check_box_3_1 = document.createElement("input");
-  check_box_3_1.setAttribute("type", "checkbox");
-  const check_box_3_2 = document.createElement("input");
-  check_box_3_2.setAttribute("type", "checkbox");
-  const check_box_3_3 = document.createElement("input");
-  check_box_3_3.setAttribute("type", "checkbox");
-
-  check_box_3_0.checked = this._button_instruction_list[0];
-  check_box_3_0.addEventListener('change', (event) => {
-    this._button_instruction_list[0] = check_box_3_0.checked;
-  });
-  check_box_3_1.checked = this._button_instruction_list[1];
-  check_box_3_1.addEventListener('change', (event) => {
-    this._button_instruction_list[1] = check_box_3_1.checked;
-  });
-  check_box_3_2.checked = this._button_instruction_list[2];
-  check_box_3_2.addEventListener('change', (event) => {
-    this._button_instruction_list[2] = check_box_3_2.checked;
-  });
-  check_box_3_3.checked = this._button_instruction_list[3];
-  check_box_3_3.addEventListener('change', (event) => {
-    this._button_instruction_list[3] = check_box_3_3.checked;
-  });
-
-  th_3.appendChild(check_box_3_0);
-  th_3.appendChild(check_box_3_1);
-  th_3.appendChild(check_box_3_2);
-  th_3.appendChild(check_box_3_3);
-  this._button_instruction_checkbox_list.push(check_box_3_0);
-  this._button_instruction_checkbox_list.push(check_box_3_1);
-  this._button_instruction_checkbox_list.push(check_box_3_2);
-  this._button_instruction_checkbox_list.push(check_box_3_3);
-
-  const th_4 = document.createElement("th");
-  const input_4 = document.createElement("input");
-  input_4.setAttribute("type", "number");
-  input_4.setAttribute("min", 1);
-  input_4.setAttribute("max", 256);
-  input_4.setAttribute("value", this._play_k_notes_int);
-  input_4.addEventListener('change', (event) => {
-    this._play_k_notes_int = event.target.value;
-  });
-  th_4.appendChild(input_4);
-
-  const th_5 = document.createElement("th");
-  const button_5_0 = document.createElement("button");
-  button_5_0.innerHTML = "delete";
-  button_5_0.addEventListener('click', () => {
-    this._to_be_remove = true;
-  });
-  const button_5_1 = document.createElement("button");
-  button_5_1.innerHTML = "duplicate";
-  button_5_1.addEventListener('click', () => {
-    daf.createGraphicalObject(new NoteUnitRow({
-      note_code_int: this._note_code_int,
-      tone_duration_millis: this._tone_duration_millis,
-      rest_duration_millis: this._rest_duration_millis,
-      // button_instruction_list: [
-      //   this._button_instruction_list[0],
-      //   this._button_instruction_list[1],
-      //   this._button_instruction_list[2],
-      //   this._button_instruction_list[3],
-      // ],
-      button_instruction_list: [...this._button_instruction_list],
-      time_unit: this._time_unit,
-      play_k_notes_int: this._play_k_notes_int,
-      audio_context: this._audio_context,
-      insert_before: this._dom_object
-    }));
-  });
-  const button_5_2 = document.createElement("button");
-  button_5_2.innerHTML = "tone";
-  button_5_2.addEventListener('click', () => {
-    this.toneNote(this._time_unit);
-  });
-  th_5.appendChild(button_5_0);
-  th_5.appendChild(document.createTextNode(' '));
-  th_5.appendChild(button_5_1);
-  th_5.appendChild(document.createTextNode(' '));
-  th_5.appendChild(button_5_2);
-
-  this._dom_object.appendChild(th_0);
-  this._dom_object.appendChild(th_1);
-  this._dom_object.appendChild(th_2);
-  this._dom_object.appendChild(th_3);
-  this._dom_object.appendChild(th_4);
-  this._dom_object.appendChild(th_5);
-
-  if(this._insert_before) {
-    animation_container_dom_object.insertBefore(this._dom_object, this._insert_before);
-    this._insert_before = null;
-  }
-  else {
-    animation_container_dom_object.appendChild(this._dom_object);
-  }
-  this._dom_object.NoteUnitRow = this;
-
-  this._dom_object.animate([
-    // keyframes
-    {
-      backgroundColor: 'green',
-      opacity: 0
-    },
-    // keyframes
-    {
-    }
-  ], {
-    duration: 400
-  });
-}
-
-NoteUnitRow.prototype.nextLogicalTick = function(logical_ticks_interval_ms, global_logical_dynamic_parameters, remove_myself)  {
-  this._time_unit = global_logical_dynamic_parameters.time_unit;
-  if(this._to_be_remove) {
-    this._to_be_remove = false;
-    remove_myself();
-    this._dom_object.animate([
-      // keyframes
-      {
-        backgroundColor: 'red'
-      },
-      // keyframes
-      {
-      }
-    ], {
-      duration: 100
-    });
-    setTimeout(() => {
-      this._dom_object.parentNode.removeChild(this._dom_object);
-    }, 100);
-  }
-}
-
-NoteUnitRow.prototype.updateGraphic = function(frames_per_second) {
-
-}
-
-NoteUnitRow.prototype.toggleButtonInstruction = function(button_index) {
-  const value = !this._button_instruction_list[button_index];
-  this._button_instruction_list[button_index] = value;
-  this._button_instruction_checkbox_list[button_index].checked = value;
-}
-
-NoteUnitRow.prototype.toneNote = function(time_unit, callback) {
-  this._dom_object.scrollIntoView({ block: 'center',  behavior: 'smooth' });
-  const oscillator = this._audio_context.createOscillator();
-  oscillator.type = 'triangle';
-  oscillator.frequency.value = Notes[this._note_code_int][0];
-  oscillator.connect(this._audio_context.destination);
-  this._dom_object.animate([
-    // keyframes
-    {
-      backgroundColor: 'green',
-      opacity: 0
-    },
-    // keyframes
-    {
-    }
-  ], {
-    duration: this._tone_duration_millis*time_unit
-  });
-  oscillator.start();
-  setTimeout(() => {
-    oscillator.stop();
-    this._dom_object.animate([
-      // keyframes
-      {
-        backgroundColor: 'red',
-        opacity: 0
-      },
-      // keyframes
-      {
-      }
-    ], {
-      duration: this._rest_duration_millis*time_unit
-    });
-    setTimeout(() => {
-      if(callback) callback(false);
-    }, this._rest_duration_millis*time_unit);
-  }, this._tone_duration_millis*time_unit);
 }
 
 function start() {
@@ -257,8 +21,8 @@ function start() {
   const audio_context = new(window.AudioContext || window.webkitAudioContext)();
   let note_unit_list = [{
     note_code_int: 37,
-    tone_duration_millis: 8,
-    rest_duration_millis: 1,
+    tone_duration_time_units: 8,
+    rest_duration_time_units: 1,
     button_instruction_list: [true, false, false, false],
     play_k_notes_int: 1,
     time_unit: time_unit,
@@ -280,7 +44,22 @@ function start() {
       });
     }
   });
-  sheet_table_daf.startAnimationAndLogicalTicking();
+
+  const game_animation = document.getElementById("game-animation");
+  const game_animation_daf = new SimpleDOMAnimationFramework({
+    animation_container_dom_object: sheet_table,
+    logical_ticks_interval_ms: 22,
+    frames_per_second: 30,
+    global_logical_dynamic_parameters: {
+      time_unit: time_unit
+    },
+    graphical_objects: [],
+    next_logical_tick: (global_logical_dynamic_parameters, create_graphical_object, update_global_logical_dynamic_parameters) => {
+      update_global_logical_dynamic_parameters({
+        time_unit: time_unit
+      });
+    }
+  });
 
   const create_note_g_binary = () => {
     let data_bytes_uint8_list = [];
@@ -303,9 +82,9 @@ function start() {
       // Note
       data_bytes_uint8_list.push(note_unit_row._note_code_int);
       // Tone duration
-      data_bytes_uint8_list.push(note_unit_row._tone_duration_millis);
+      data_bytes_uint8_list.push(note_unit_row._tone_duration_time_units);
       // Rest duration
-      data_bytes_uint8_list.push(note_unit_row._rest_duration_millis);
+      data_bytes_uint8_list.push(note_unit_row._rest_duration_time_units);
       // Instruction
       const button_instuction_4bit_int = (note_unit_row._button_instruction_list[0]?1:0)+2*(note_unit_row._button_instruction_list[1]?1:0)+4*(note_unit_row._button_instruction_list[2]?1:0)+8*(note_unit_row._button_instruction_list[3]?1:0);
       const play_k_notes_4bit_int = note_unit_row._play_k_notes_int;
@@ -349,9 +128,9 @@ function start() {
       // Note
       note_unit.note_code_int = unit8_array[pointer++];
       // Tone duration
-      note_unit.tone_duration_millis = unit8_array[pointer++];
+      note_unit.tone_duration_time_units = unit8_array[pointer++];
       // Rest duration
-      note_unit.rest_duration_millis = unit8_array[pointer++];
+      note_unit.rest_duration_time_units = unit8_array[pointer++];
 
       // Instruction
       const instruction_8bit = unit8_array[pointer++];
@@ -513,8 +292,8 @@ function start() {
               // console.log('quarters: ', item.getElementsByTagName('type')[0].innerHTML);
               new_note_unit_list.push({
                 note_code_int: (item.getElementsByTagName('rest').length)?0:12*octave+step+alter,
-                tone_duration_millis: duration*2,
-                rest_duration_millis: 1,
+                tone_duration_time_units: duration*2,
+                rest_duration_time_units: 1,
                 button_instruction_list: [false, false, false, false],
                 play_k_notes_int: 1,
                 time_unit: new_time_unit,
